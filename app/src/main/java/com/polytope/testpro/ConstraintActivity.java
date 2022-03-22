5c399ffe6e48d8ca835b72526e2684e8dde14a93
杨少云 <combat_yang@163.com>
2022-03-22 16:03:31 +0800

    添加签名和ConstraintLayout布局

---
+++ app/src/main/java/com/polytope/testpro/ConstraintActivity.java
@@ -0,0 +1,11 @@
+package com.polytope.testpro;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+/**
+ * @author: 雪豹高科
+ * @time: 2022-3-17
+ */
+public class ConstraintActivity extends AppCompatActivity {
+
+}
